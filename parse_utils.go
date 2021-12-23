5e531cb7e14a7a627d9db86fbc5c8dd353504e7b
Andrei Pelinescu-Onciul <andrei@iptel.org>
2021-12-23 13:26:32 +0100

    license: some bsd, some source-available

--- parse_utils.go
+++ parse_utils.go
@@ -1,7 +1,7 @@
 // Copyright 2021 Intuitive Labs GmbH. All rights reserved.
 //
-// Use of this source code is governed by a source-available license
-// that can be found in the LICENSE.txt file in the root of the source
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE_BSD.txt file in the root of the source
 // tree.
 
 package httpsp
